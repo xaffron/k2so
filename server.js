76b0221c8e8321ad01666ae62d1a153c4aa61cf3
xaffron <xaffron@users.noreply.github.com>
2017-06-16 14:01:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const Slapp = require('slapp')
 const ConvoStore = require('slapp-convo-beepboop')
 const Context = require('slapp-context-beepboop')
+const needle = require('needle')
 
 // use `PORT` env var on Beep Boop - default to 3000 locally
 var port = process.env.PORT || 3000
@@ -24,6 +25,133 @@ I will respond to the following messages:
 \`<type-any-other-text>\` - to demonstrate a random emoticon response, some of the time :wink:.
 \`attachment\` - to see a Slack attachment message.
 `
+let baseUrl = process.env.BASE_URL || `https://beepboophq.com/proxy/${process.env.BEEPBOOP_PROJECT_ID}`
+let config = module.exports = {
+  // HTTP port
+  port: process.env.PORT || 4000,
+
+  // External base URL
+  base_url: baseUrl,
+
+  // Slapp config
+  debug: !!process.env.DEBUG,
+  slapp_colors: true,
+  slapp_log: true,
+  slack_verify_token: process.env.SLACK_VERIFY_TOKEN,
+
+  // Beep Boop Persist API provider (beepboop, fs, memory)
+  persist_provider: process.env.PERSIST_PROVIDER || 'beepboop',
+
+  // Beep Boop Project Id and Token for Chronos API
+  beepboop_project_id: process.env.BEEPBOOP_PROJECT_ID,
+  beepboop_token: process.env.BEEPBOOP_TOKEN,
+
+  validate: () => {
+    let required = ['beepboop_token']
+
+    required.forEach((prop) => {
+      if (!config[prop]) {
+        throw new Error(`${prop.toUpperCase()} required but missing`)
+      }
+    })
+    return config
+  }
+}
+
+//BEGIN CHRONOS
+module.exports = (config) => {
+  return new Chronos(config)
+}
+
+class Chronos {
+  constructor (config) {
+    if (!config.beepboop_token) throw new Error('beepboop_token required')
+    if (!config.beepboop_project_id) throw new Error('beepboop_project_id required')
+    this.token = config.beepboop_token
+    this.project_id = config.beepboop_project_id
+    this.base = config.base || 'https://beepboophq.com/api/v1/chronos'
+  }
+
+  list (callback) {
+    this._get(`${this.base}/tasks`, callback)
+  }
+
+  active (callback) {
+    this._get(`${this.base}/tasks?inactive=false`, callback)
+  }
+
+  inactive (callback) {
+    this._get(`${this.base}/tasks?inactive=true`, callback)
+  }
+
+  create (data, callback) {
+    needle.post(`${this.base}/tasks`, data, this._baseOptions(), (err, resp) => {
+      if (err) return callback(err)
+      if (resp.statusCode !== 201) {
+        return callback(new Error(`unsuccesful status code ${resp.statusCode}`))
+      }
+      callback(null, resp.body)
+    })
+  }
+
+  scheduleSyntheticEvent (msg, cron, type, payload, callback) {
+    let ts = Date.now() + ''
+
+    this.create({
+      schedule: cron,
+      url: `https://beepboophq.com/proxy/${this.project_id}/slack/event`,
+      method: 'POST',
+      headers: {
+        'BB-Enrich': `slack_team_id=${msg.meta.team_id}`
+      },
+      payload: {
+        token: msg.body.token,
+        team_id: msg.meta.team_id,
+        type: 'event_callback',
+        event: {
+          ts: ts,
+          event_ts: ts,
+          type: type,
+          payload: payload,
+          user: msg.meta.user_id,
+          channel: msg.meta.channel_id
+        }
+      }
+    }, callback)
+  }
+
+  delete (id, callback) {
+    console.log(`${this.base}/tasks/${id} - ${this.token}`)
+    needle.delete(`${this.base}/tasks/${id}`, null, this._baseOptions(), (err, resp) => {
+      if (err) return callback(err)
+      if (resp.statusCode !== 200) {
+        return callback(new Error(`unsuccesful status code ${resp.statusCode}`))
+      }
+      callback(null, resp.body)
+    })
+  }
+
+  _get (url, callback) {
+    needle.get(url, this._baseOptions(), (err, resp) => {
+      if (err) return callback(err)
+      if (resp.statusCode !== 200) {
+        return callback(new Error(`unsuccesful status code ${resp.statusCode}`))
+      }
+      callback(null, resp.body)
+    })
+  }
+
+  _baseOptions () {
+    return {
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      },
+      json: true
+    }
+  }
+}
+
+//END CHRONOS
 
 //*********************************************
 // Setup different handlers for messages
