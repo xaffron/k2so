40bf6e548c49efb780adc92bc45281347718d61a
xaffron <xaffron@users.noreply.github.com>
2017-06-16 17:57:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const Context = require('slapp-context-beepboop')
 var officers = ['U2A3YP9MH','eoa',-5,
                    'U2B6M7MSR','schwefumbler',-6,
                    'U4FA4LE5N','alphonsis',-7,
-                   'XXX','bluemoose',-4,
-                   'YYY','yer.reklaw',-8]
+                   'XXX','bluemoose',-6,
+                   'YYY','yer.reklaw',-7]
 
 // use `PORT` env var on Beep Boop - default to 3000 locally
 var port = process.env.PORT || 3000
@@ -159,15 +159,15 @@ slapp.message('.*', ['direct_message', 'direct_mention', 'mention', 'ambient'], 
       let dt = new Date(Date.now()+3600000*offset);
       let hr = dt.getHours();
       let answer = 'Captain ' + uName + ', it is ' + hr + '00 hours.';
-      if (hr==11 || hr==15 || hr==17 || hr==19 || hr==20 || hr==22) {
+      if (hr==11 || hr==15 || hr==19 || hr==20 || hr==22) {
         answer += '  This is only a drill.  If there were a flash event today, you would need to report to duty immediately.';
+        msg.say({
+          channel: 'G2BHD8H0F',
+          link_names: true,
+          as_user: true,
+          text: answer
+        });
       }
-//        link_names: true,
-      msg.say({
-        channel: 'G2BHD8H0F',
-        as_user: true,
-        text: answer
-      });
     }
   }
 })
