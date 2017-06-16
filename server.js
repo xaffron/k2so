3900add012a91639dc05fd3f5cbb1077e0a3c702
xaffron <xaffron@users.noreply.github.com>
2017-06-16 12:02:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -113,6 +113,10 @@ slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
   }
 })
 
+slapp.message('goodnight', 'mention', (msg) => {
+  msg.say('sweet dreams :crescent_moon: ')
+})
+
 // attach Slapp to express server
 var server = slapp.attachToExpress(express())
 
