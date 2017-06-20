b7c87c7c00de37a649fdf4e04387a08a3e841f97
xaffron <xaffron@users.noreply.github.com>
2017-06-20 17:10:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ const officers = [
   'U2AFRRVL1','bluemoose',-5,
   'U2A6642T1','yer.reklaw',-7,
   'U4U5MRWGJ','blackvseries',4,
-  'U2AKRH076','tanga',-4
+  'U2AKRH076','tanga',-4,
+  'U2AC1PF29','jonosmol',-7
                  ]
 
 //const officers = ['U2A3YP9MH','eoa',-5]
